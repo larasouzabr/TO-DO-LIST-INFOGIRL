8bde3de8e9f71d79243b5ca12921123b433c1896
Lara Souza <laragabriellysouzabatista@gmail.com>
2022-08-24 15:10:16 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './class/Todo';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loginPage';
+  toDos: Todo[] = [];
+  newTodo!: string;
+
+  saveToDo(){
+    if(this.newTodo){
+    let todo = new Todo();
+    todo.name = this.newTodo;
+    todo.isCompleted = false;
+    this.toDos.push(todo);
+    this.newTodo = '';
+    }
+    else {
+      alert("Adicione uma tarefa");
+    }
+  }
+  doneToDo(id:number){
+    this.toDos[id].isCompleted = !this.toDos[id].isCompleted;
+  }
+  
+  removeToDo(id:number){
+    this.toDos = this.toDos.filter((v,i) => i !== id);
+  }
+
 }
